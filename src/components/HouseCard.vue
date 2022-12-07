0a782ee8795681e5973d295b64448251ebdf4115
Derik Banning <deriklee451@gmail.com>
2022-12-07 15:34:50 -0700

    Initial commit

---
+++ src/components/HouseCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="card text-center elevation-2 selectable" @click="goTo">
+        <img class="img-fluid rounded square-bottom" :src="car.imgUrl" alt="">
+        <h5>{{ }} | {{ }} | {{ }}</h5>
+        <h5>${{ car.price }}</h5>
+        <div class="p-2 text-start">{{ }}</div>
+        <button v-if="(car.creatorId == account.id)" @click="removeCar(car.id)"
+            class="btn btn-danger delete-btn rounded-pill"><i class="  px-2 mdi mdi-delete-forever"></i></button>
+        <div v-if="car.creator" class="creator text-end p-1">
+            <img :src="car.creator.picture" alt="">
+            <span>{{ car.creator.name }}</span>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import { AppState } from '../AppState';
+import { computed, reactive, onMounted } from 'vue';
+export default {
+    props: { house: Object },
+    setup(props) {
+        return {}
+    }
+};
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
--- src/pages/HousesPage.vue
+++ src/pages/HousesPage.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="component">
-    Houses Page
+  <div class="container-fluid">
+    <h1 class="text-center text-primary"> Houses</h1>
+    <div class="row justify-content-center">
+      <div class="col-8 bg-white p-3 elevation-1 rounded">
+        <!-- <HouseForm /> -->
+      </div>
+    </div>
 
+    <div class="row">
+
+      <div v-for="h in houses" class="col-md-4 p-4">
+        <HouseCard :house="h" />
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -9,10 +21,12 @@
 <script>
 import { AppState } from '../AppState';
 import { computed, reactive, onMounted } from 'vue';
-export default {
-  setup() {
-    return {}
-  }
+import HouseForm from '../components/HouseForm.vue.js';
+  rt defau lt {
+   tup() {
+    rn {};
+  
+  components: { HouseForm }
 };
 </script>
 
